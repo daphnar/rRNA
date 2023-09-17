e2763e7d03edc355d53fb4e6f155b06ca0664539
Daphna Rothschild <dafna.aloni@gmail.com>
2023-09-16 23:02:48 -0700

    Update RGA_implementation.py

--- RGA_implementation.py
+++ RGA_implementation.py
@@ -7,12 +7,6 @@ def run_needle_on_region_variants(region_atlas,output_dir,tmp_dir,exe_path=''):
     other_variants = seq_files[1:]
     all_gap_ref_positions = []
     for idx,variant_file in enumerate(other_variants):
-        ##################OLD params:
-        # command = '%s -asequence %s -bsequence %s -datafile EDNAFULL -gapopen 2' \
-        #           ' -gapextend 10 -snucleotide1' \
-        #           ' -snucleotide2 -outfile %s -aformat fasta' % (
-        #           executable, ref_file, variant_file, variant_file + '.needle')
-        ##################New params:
         command = '%s -asequence %s -bsequence %s -datafile EDNAFULL -gapopen 10' \
                   ' -gapextend 0.5 -snucleotide1' \
                   ' -snucleotide2 -outfile %s -aformat fasta' % (
@@ -25,14 +19,10 @@ def run_needle_on_region_variants(region_atlas,output_dir,tmp_dir,exe_path=''):
         ref_record_gap = list(ref_record_s[ref_record_s == '-'].index.values)
         for gap_n, gap_pos in enumerate(ref_record_gap):
             ref_record_gap[gap_n] = gap_pos - gap_n  # gaps make the sequence longer which we correct
-        # all_gap_ref_positions+=ref_record_gap
         how_many_times = pd.Series(ref_record_gap).value_counts().astype(int)
         all_gap_ref_positions.append(how_many_times)
         if idx%5000==0:
             print('5000 done')
-    # all_gap_ref_positions=list(set(all_gap_ref_positions))
-    # all_gap_ref_positions.sort()
-    # print('WTF %s'%(len(all_gap_ref_positions)))
     try:
         all_gap_ref_positions = pd.concat(all_gap_ref_positions).sort_index()
     except ValueError:
@@ -46,22 +36,16 @@ def run_needle_on_region_variants(region_atlas,output_dir,tmp_dir,exe_path=''):
         'gap_size']
     all_gap_ref_positions_max.to_csv(
         os.path.join(output_dir, region_atlas.replace('dereplication.fa', 'reference_gap_size_per_position.csv')))
-    # #Todo: uncomment above and remove line below
-    # all_gap_ref_positions_max = pd.read_csv(os.path.join(output_dir, region_atlas.replace('dereplication.fa', 'reference_gap_size_per_position.csv')),index_col=0)['gap_size']
     reference_seq = list(SeqIO.parse(ref_file, 'fasta'))[0]
     ref_nuc = list(reference_seq.seq)
     seq_id = reference_seq.id
     seq_description = reference_seq.description
-    # ref_with_gaps=pd.Series(['N']*(len(ref_nuc)+len(all_gap_ref_positions)))
     ref_with_gaps = pd.Series(['N'] * (len(ref_nuc) + all_gap_ref_positions_max.sum()))
     correction_gap_pos = {}
-    # for gap_n,gap_pos in enumerate(all_gap_ref_positions):
     for gap_n, gap_pos in enumerate(all_gap_ref_positions_max.index):
-        # correction_gap_pos[gap_pos]=gap_pos+gap_n #gaps make the sequence longer which we correct
         correction_gap_pos[gap_pos] = gap_pos + all_gap_ref_positions_max[:gap_n].sum().astype(int)
         ref_with_gaps[
         correction_gap_pos[gap_pos]:(correction_gap_pos[gap_pos] + all_gap_ref_positions_max[gap_pos])] = '-'
-    # ref_with_gaps[correction_gap_pos.values()] = '-'
     ref_with_gaps[ref_with_gaps == 'N'] = ref_nuc
     all_aligned_records.append(
         SeqRecord.SeqRecord(Seq.Seq("".join(list(ref_with_gaps.values))), id=seq_id, description=seq_description))
@@ -75,25 +59,17 @@ def run_needle_on_region_variants(region_atlas,output_dir,tmp_dir,exe_path=''):
         how_many_times = pd.Series(currect_ref_record_gap).value_counts().astype(int)
         need_to_put_gap = []
         aligned_with_gaps = pd.Series(['N'] * len(ref_with_gaps))
-        #good=True#REMOVE
         for gap_pos in correction_gap_pos.keys():
             if gap_pos not in currect_ref_record_gap:
-                # need_to_put_gap.append(correction_gap_pos[gap_pos])
                 how_map_gaps_in_pos = all_gap_ref_positions_max[gap_pos]
                 corrected_pos = correction_gap_pos[gap_pos]
                 aligned_with_gaps[corrected_pos:corrected_pos + how_map_gaps_in_pos] = '-'
             else:
                 how_map_gaps_in_pos = all_gap_ref_positions_max[gap_pos] - how_many_times[gap_pos]
                 assert how_map_gaps_in_pos >= 0
-                # if how_map_gaps_in_pos < 0:
-                #     good=False
-                #     break
                 if how_map_gaps_in_pos > 0:
                     corrected_pos = correction_gap_pos[gap_pos]
                     aligned_with_gaps[corrected_pos:corrected_pos + how_map_gaps_in_pos] = '-'
-        # if not good:
-        #     continue
-        # aligned_with_gaps[need_to_put_gap]='-'
         aligned_nuc = list(aligned_seqs[1].seq)
         aligned_id = aligned_seqs[1].id
         aligned_description = aligned_seqs[1].description
@@ -111,5 +87,4 @@ def run_needle_on_region_variants(region_atlas,output_dir,tmp_dir,exe_path=''):
             raise ValueError
     with open(output_region_alignment, 'w') as f_h:
         SeqIO.write(all_aligned_records, f_h, 'fasta')
-    print(os.path.basename(region_atlas), 'missing %s seqs' % double_gap_reference_n)
 
